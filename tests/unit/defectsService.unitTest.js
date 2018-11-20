8eb2125f4a9b6c12e05c0685f9200d44380aeadc
George Mihai Lazu <glazu@deloittece.com>
2018-11-20 14:40:27 +0200

    Added config files

--- tests/unit/defectsService.unitTest.js
+++ tests/unit/defectsService.unitTest.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const defectsService = require('../../services/defectsService')
+const defectsService = require('../../src/services/defectsService')
 
 describe('defectsService', () => {
   it('should fetch all defects from the database', async () => {
