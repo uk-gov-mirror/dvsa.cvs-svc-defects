52c9698b6069823b67e557e24e04415b626a65b3
oalexandru <oalexandru@deloittece.com>
2018-11-29 01:50:54 +0200

    Almost finished refactoring in order to be testable

--- src/models/Defects.js
+++ src/models/Defects.js
@@ -1,28 +1,15 @@
-const HTTPResponseStatus = require('../models/HTTPResponseStatus')
+const AWS = require('aws-sdk')
 const config = require('../config/config')
+const dbClient = new AWS.DynamoDB.DocumentClient(
+  (config.ENV === 'local') ? { region: config.OFFLINE.DYNAMODB_REGION, endpoint: config.OFFLINE.DYNAMODB_ENDPOINT } : {})
 
 class Defects {
-  constructor (dbClient) {
-    this.dbClient = dbClient;
+  constructor () {
     (config.ENV === 'local') ? (this.tableName = `cvs-${config.ENV}-${config.OFFLINE.COMPONENT}-defects`) : (this.tableName = `cvs-${config.ENV}-${config.COMPONENT}-defects`)
   }
 
   getAll () {
-    console.log(this.dbClient.scan({ TableName: this.tableName }))
-
-    return this.dbClient.scan({ TableName: this.tableName })
-      .promise()
-      .then((result) => {
-        if (result.$response.error) {
-          throw new HTTPResponseStatus(result.$response.error.statusCode, result.$response.error.message)
-        }
-
-        if (result.Count === 0 || result.Items === undefined) {
-          throw new HTTPResponseStatus(404, { error: 'No resources match the search criteria.' })
-        }
-
-        return result.Items
-      })
+    return dbClient.scan({ TableName: this.tableName }).promise()
   }
 }
 
---
+++ src/models/DefectsMock.js
@@ -0,0 +1,12 @@
+class DefectsMock {
+
+  constructor (jsonMock) {
+    this.jsonMock = jsonMock;
+  }
+
+  getAll () {
+    return Promise.resolve(this.jsonMock)
+  }
+}
+
+module.exports = DefectsMock
