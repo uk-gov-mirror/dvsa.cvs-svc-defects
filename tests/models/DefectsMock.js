2b1e87a1495148c45866257e93452463426073ff
oalexandru <oalexandru@deloittece.com>
2018-11-29 16:36:19 +0200

    Added test for database down

--- tests/models/DefectsMock.js
+++ tests/models/DefectsMock.js
@@ -1,13 +1,16 @@
 class DefectsMock {
-  constructor (defectRecordsMock, numberOfrecords, numberOfScannedRecords) {
+  constructor (defectRecordsMock, numberOfrecords, numberOfScannedRecords, isDatabaseOn) {
     this.responseObject = {
       Items: defectRecordsMock,
       Count: numberOfrecords,
       ScannedCount: numberOfScannedRecords
     }
+    this.isDatabaseOn = isDatabaseOn
   }
 
   getAll () {
+    if (!this.isDatabaseOn) { return Promise.reject(this.responseObject) }
+
     return Promise.resolve(this.responseObject)
   }
 }
--- tests/services/defectsService.unitTest.js
+++ tests/services/defectsService.unitTest.js
@@ -6,7 +6,7 @@ const HTTPResponseStatus = require('../../src/models/HTTPResponseStatus')
 describe('getDefectList', () => {
   it('should return a populated response in case db call returns data', () => {
     const defectRecordsMock = { item: 'testItem' }
-    var defectsMock = new DefectsMock(defectRecordsMock, 1, 1)
+    var defectsMock = new DefectsMock(defectRecordsMock, 1, 1, true)
     var defectsService = new DefectsService(defectsMock)
 
     return defectsService.getDefectList()
@@ -20,7 +20,7 @@ describe('getDefectList', () => {
 
   it('should return 404-No resources match the search criteria if db returns empty data', () => {
     const defectRecordsMock = {}
-    var defectsMock = new DefectsMock(defectRecordsMock, 0, 0)
+    var defectsMock = new DefectsMock(defectRecordsMock, 0, 0, true)
     var defectsService = new DefectsService(defectsMock)
 
     return defectsService.getDefectList()
@@ -35,7 +35,7 @@ describe('getDefectList', () => {
 
   it('should return 404-No resources match the search criteria if db return null data', () => {
     const defectRecordsMock = undefined
-    var defectsMock = new DefectsMock(defectRecordsMock, 0, 0)
+    var defectsMock = new DefectsMock(defectRecordsMock, 0, 0, true)
     var defectsService = new DefectsService(defectsMock)
 
     return defectsService.getDefectList()
@@ -45,6 +45,19 @@ describe('getDefectList', () => {
         expect(errorResponse).to.be.instanceOf(HTTPResponseStatus)
         expect(errorResponse.statusCode).to.equal(404)
         expect(errorResponse.body).to.equal('No resources match the search criteria.')
+      })
+  })
+
+  it('should return 500-Internal Server Error if db not returning response', () => {
+    var defectsMock = new DefectsMock(null, 0, 0, false)
+    var defectsService = new DefectsService(defectsMock)
+
+    return defectsService.getDefectList()
+      .then(() => {})
+      .catch((errorResponse) => {
+        expect(errorResponse).to.be.instanceOf(HTTPResponseStatus)
+        expect(errorResponse.statusCode).to.be.equal(500)
+        expect(errorResponse.body).to.equal('Internal Server Error')
       })
   })
 })
