e6378c2c1d1bcabad2cf34963712518ed8bc51a4
Arup Banerjee <arupdvsa@alanaamy.net>
2019-07-31 16:23:01 +0100

    typescript migration and quality stages 1

--- tests/unit/deleteDefectList.unitTest.ts
+++ tests/unit/deleteDefectList.unitTest.ts
@@ -1,18 +1,23 @@
 import { DefectsService } from "../../src/services/DefectsService";
-import { Injector } from "../../src/models/injector/Injector";
-import { DefectsDAOMock } from "../models/DefectsDAOMock";
 import { expect } from "chai";
 import { HTTPError } from "../../src/models/HTTPError";
 
-describe("deleteDefectList", () => {
+describe("when service method deleteDefectList is called", () => {
     describe("when database is off", () => {
-        context("when db does not return response", () => {
+        context("when defectsDAO deleteMultiple returns a rejected promise", () => {
             it("should return 500-Internal Server Error", () => {
-                const defectsService: DefectsService = Injector.resolve<DefectsService>(DefectsService, [DefectsDAOMock]);
-                DefectsDAOMock.isDatabaseOn = false;
-                DefectsDAOMock.defectRecordsMock = require("../resources/defects.json");
+                const MockDefectsDAO = jest.fn().mockImplementation(() => {
+                    return {
+                        deleteMultiple: () => {
+                            return Promise.reject({});
+                        }
+                    };
+                });
 
-                return defectsService.deleteDefectList(DefectsDAOMock.defectRecordsMock)
+                const mockDefectsDAO = new MockDefectsDAO();
+                const defectsService: DefectsService = new DefectsService(mockDefectsDAO);
+
+                return defectsService.deleteDefectList([])
                     .catch((errorResponse) => {
                         expect(errorResponse).to.be.instanceOf(HTTPError);
                         expect(errorResponse.statusCode).to.be.equal(500);
@@ -23,16 +28,50 @@ describe("deleteDefectList", () => {
     });
 
     describe("when database is on", () => {
-        context("when deleting a valid defects array", () => {
-            it("should return 200", () => {
-                const defectsService: DefectsService = Injector.resolve<DefectsService>(DefectsService, [DefectsDAOMock]);
-                DefectsDAOMock.isDatabaseOn = true;
-                DefectsDAOMock.defectRecordsMock = require("../resources/defects.json");
+        context("when defectsDAO deleteMultiple yields promise with unprocessed defect items", () => {
+            it("should return 200 along with unprocessed items", () => {
+                const expectedUnprocessedDefects = {
+                    UnprocessedItems: [{ imNumber: "1" }],
+                    Count: 1
+                };
 
-                return defectsService.deleteDefectList(DefectsDAOMock.defectRecordsMock)
-                    .then((result: any) => {
-                          expect(Object.keys(result).length).to.equal(0);
-                          expect(result.constructor).to.equal(Object);
+                const MockDefectsDAO = jest.fn().mockImplementation(() => {
+                    return {
+                        deleteMultiple: () => {
+                            return Promise.resolve(expectedUnprocessedDefects);
+                        }
+                    };
+                });
+
+                const mockDefectsDAO = new MockDefectsDAO();
+                const defectsService: DefectsService = new DefectsService(mockDefectsDAO);
+                return defectsService.deleteDefectList(["1"])
+                    .then((returnedRecords: any) => {
+                        expect(Object.keys(returnedRecords).length).to.equal(1);
+                        expect(returnedRecords.constructor).to.equal(Array);
+                        expect(returnedRecords).to.not.equal(undefined);
+                        expect(returnedRecords).to.not.equal({});
+                        expect(returnedRecords).to.deep.equal(expectedUnprocessedDefects.UnprocessedItems);
+                        expect(returnedRecords.length).to.be.equal(expectedUnprocessedDefects.UnprocessedItems.length);
+                    });
+            });
+        });
+
+        context("when defectsDAO deleteMultiple yields promise with no unprocessed defect items", () => {
+            it("should return HTTP 200", () => {
+                const MockDefectsDAO = jest.fn().mockImplementation(() => {
+                    return {
+                        deleteMultiple: () => {
+                            return Promise.resolve({});
+                        }
+                    };
+                });
+
+                const mockDefectsDAO = new MockDefectsDAO();
+                const defectsService: DefectsService = new DefectsService(mockDefectsDAO);
+                defectsService.deleteDefectList(["1"])
+                    .then((returnedRecords: any) => {
+                        expect(returnedRecords).to.equal(undefined);
                     });
             });
         });
