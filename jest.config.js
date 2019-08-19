42bddbc8803d0965d950cc8fdb5bc694db16bd70
Arup Banerjee <arupdvsa@alanaamy.net>
2019-08-19 15:50:16 +0100

    apply changes for typescript sonar-scanner

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,11 @@ module.exports = {
   ],
   setupFiles: [
     "jest-plugin-context/setup"
-  ]
+  ],
+  moduleFileExtensions: ["js", "ts"],
+  testResultsProcessor: "jest-sonar-reporter",
+  transform: {
+    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+  },
+  testMatch: ["<rootDir>/tests/**/*.test.ts"]
 }
