e8dfcccc4b1134ec2dfe68073a68f628258c0e94
khodder <khodder@gmail.com>
2019-09-05 16:30:41 +0100

    CVSB-3331 remove eslint and associated vulnerabilities

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   moduleFileExtensions: ["js", "ts"],
   testResultsProcessor: "jest-sonar-reporter",
   transform: {
-    "^.+\\.ts$": "<rootDir>/node_modules/ts-jest/preprocessor.js",
+    "^.+\\.ts$": "ts-jest",
   },
   testMatch: ["<rootDir>/tests/**/*.test.ts"]
 }
